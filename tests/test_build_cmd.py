e89faccae709f15b28bdae83021b67a0fb5c4763
mca36 <89984970+marcus-24@users.noreply.github.com>
2022-04-08 16:55:02 -0400

    Update test_build_cmd.py
    added setUp function in test_build_cmd.py

--- tests/test_build_cmd.py
+++ tests/test_build_cmd.py
@@ -1,29 +1,39 @@
 import unittest
 from struct import pack
 
-from iotools.build_cmd import BuildCommands, DATA_CMDS, RESP_CMDS, SET_RESP_HEAD
+from iotools.build_cmd import BuildCommands, DATA_CMDS, RESP_CMDS, SET_RESP_HEAD, SET_SLOT
 
 
 class BuildCmdTests(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.bldg_cmd = BuildCommands()
+
     def test_all_responses(self):
+        self.bldg_cmd.resp_cmds = RESP_CMDS
         self.assertEqual(pack('B', SET_RESP_HEAD) + pack('>I', 127),
-                         BuildCommands(resp_cmds=RESP_CMDS).pack_response_header())
+                         self.bldg_cmd.pack_response_header())
 
     def test_invalid_response(self):
         resp_cmd = {'fake response': {"command": 900,
                                       "unpack": ">3f",
                                       "raw length": 12}
                     }
+        
         with self.assertRaises(ValueError):
-            BuildCommands(resp_cmds=resp_cmd)
+            self.bldg_cmd.resp_cmds = resp_cmd
 
     def test_fill_all_streaming_slots_with_commands(self):
-        eight_cmds = {key: value for idx, (key, value) in enumerate(DATA_CMDS.items()) if idx < 8}
-        hex_cmds = [80] + [cmd["command"] for _, cmd in eight_cmds.items()]
-        self.assertEqual(BuildCommands(data_cmds=eight_cmds).pack_data_cmds(),
-                          pack('BBBBBBBBB', *hex_cmds)
-                          )
+        '''Set up data commands'''
+        n_cmds = 8  # number of commands to extract
+        eight_cmds = {key: value for idx, (key, value) in enumerate(DATA_CMDS.items()) if idx < n_cmds}
+
+        '''Set hex commands'''
+        hex_cmds = [SET_SLOT] + [cmd["command"] for _, cmd in eight_cmds.items()]
+        self.bldg_cmd.data_cmds = eight_cmds
+
+        self.assertEqual(self.bldg_cmd.pack_data_cmds(),
+                         pack('BBBBBBBBB', *hex_cmds))
 
     def test_invalid_data_command(self):
         data_cmd = {'fake sensor': {"command": 1020,
@@ -32,12 +42,12 @@ class BuildCmdTests(unittest.TestCase):
                                     "raw length": 15}
                     }
         with self.assertRaises(ValueError):
-            BuildCommands(data_cmds=data_cmd)
+            self.bldg_cmd.data_cmds = data_cmd
 
     def test_too_many_streaming_slots(self):
         
         with self.assertRaises(ValueError):
-            BuildCommands(data_cmds=DATA_CMDS)
+            self.bldg_cmd.data_cmds = DATA_CMDS
 
 
 if __name__ == '__main__':
